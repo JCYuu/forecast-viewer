187362c166d5fa63ada1450868e73fa9d89a13dc
JCY <hrjcgamer@gmail.com>
2023-12-07 17:30:48 -0500

    this

---
+++ src/components/Count.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "Count",
+  data(){
+    return {
+      counter: 1
+    }
+  },
+  methods:{
+    increaseCount (){
+      this.counter++;
+    }
+  }
+}
+</script>
+
+<template>
+  <span>Count: {{counter}}</span>
+  <button @click="increaseCount">Sube esto</button>
+</template>
+
+<style scoped>
+
+</style>
