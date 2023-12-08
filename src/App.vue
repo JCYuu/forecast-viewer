d1fde8470499395a5508664a8eb6a748a4ba0bad
JCY <hrjcgamer@gmail.com>
2023-12-08 01:37:35 -0500

    this

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,21 @@ import Count from "@/components/Count.vue";
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    Variables
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <Count />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <form action="submit">
+
+
+    </form>
+  <div class="wrapper">
+    <Count />
+  </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
