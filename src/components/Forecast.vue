7c9b60a204ad88bd1e1a589a0097094d689420bb
JCY <hrjcgamer@gmail.com>
2023-12-09 17:40:37 -0500

    Basically finished Forecasts, added the next 3 days and the options for the menu drawer

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,17 +1,53 @@
 <script>
   import axios from "axios";
-  const token = "1|RWGuH9pEsMk2CudoOKiYP3sY7UXFaG6vn6eCwmqm47dc6971"
+  const token = "1|RXFFpicTbNIZxrbC25ZTwkhhS3yoHdq4nVpJOQtz92d4ade7"
   const config = {
     headers: { Authorization: `Bearer ${token}`},
+  }
+
+/*  function parseFutureForecasts(forecasts){
+    this.nextForecast = {}
+    for (const date in forecasts) {
+      this.nextForecast[0].append({})
+    }
+  }*/
+
+  function parseFutureForecasts(forecasts){
+    console.log(forecasts)
+    let currentDate = forecasts[0]['date'];
+    console.log(currentDate);
+    let futureForecasts = {};
+    let forecastIndex = 0;
+    let regionIndex = 0;
+    let regions = {};
+    for (const data of forecasts) {
+    if (data['date'] !== currentDate){
+      futureForecasts[forecastIndex] = regions;
+      currentDate = data['date'];
+      regions = {};
+      forecastIndex++;
+      regionIndex = 0;
+    }
+      regions[regionIndex] = data;
+      console.log(data);
+      regionIndex++;
+
+    }
+    console.log(futureForecasts)
+    return futureForecasts;
   }
   export default {
     data(){
       return {
         todayForecast: null,
-        text: "",
         nextForecast: null,
-        weatherStatus: {1: "Soleado", 2: "Nublado", 3: "Lluvioso", 4: "Tormenta"},
-          weatherIcons: { 1: "mdi-weather-sunny", 2: "mdi-weather-cloudy", 3: "mdi-weather-rainy", 4: "mdi-weather-lightning" }
+        nextForecastDate: null,
+        weatherStatus: {1: "Soleado", 2: "Soleado con nubes", 3: "Nublado",
+                        4: "Lluvia ligera", 5: "Lluvioso", 6: "Chubasco",
+                        7: "Fuertes Lluvias", 8: "Tormenta"},
+        weatherIcons: { 1: "mdi-weather-sunny", 2: "mdi-weather-partly-cloudy", 3: "mdi-weather-cloudy",
+                        4: "mdi-weather-partly-rainy", 5: "mdi-weather-rainy", 6: "mdi-weather-pouring",
+                        7: "mdi-weather-lightning-rainy", 8: "mdi-weather-hurricane-outline"}
       }
     },
     methods: {
@@ -20,18 +56,8 @@
           const response = await axios('http://localhost:8000/api/weather', config);
           console.log(response.data);
           this.todayForecast = response.data.today.data;
-          this.nextForecast = response.data.forecasts.data
-
-
-          console.log('Today:');
-          for (const region in this.todayForecast) {
-            let regionData = this.todayForecast[region]
-            console.log(`${this.todayForecast[region]["region"]}`)
-            for (const data in regionData) {
-              if(data === 'region') continue;
-              console.log(`${data} : ${regionData[data]}`)
-            }
-          }
+          this.nextForecast = parseFutureForecasts(response.data.forecasts.data);
+          console.log(this.nextForecast)
 
 
           // console.log(`Today: ${JSON.stringify(this.forecast.data)}`);
@@ -59,89 +85,74 @@
 
 <template>
   <div class="today-forecast pa-3 align-content-center" v-if="todayForecast !== null">
-    <h2>Pronóstico para el día de hoy {{ todayForecast[0]['date'] }}</h2>
-    <div class="forecast-container align-content-center pa-3 d-md-flex">
-      <v-card class="mx-md-3 px-1 mx-auto my-3 w-75" min-width="240">
+    <h2>Fecha de hoy: {{ todayForecast[0]['date'] }}</h2>
+    <div class="forecast-container align-content-center justify-space-between pa-3 d-md-flex flex-wrap">
+      <v-card v-for="region in todayForecast" class="mx-md-3 px-1 mx-auto my-3" min-width="280">
         <template v-slot:title>
-          <v-icon icon="mdi-weather-partly-cloudy"></v-icon>
-          <v-card-item title="Región Norte"></v-card-item>
+          <v-icon icon="mdi-clouds"></v-icon>
+          <v-card-item>Región {{ region["region"] }}</v-card-item>
         </template>
         <v-card-text class="py-0">
           <v-row class="align-center">
             <v-col class="text-h2">
-              {{ todayForecast[0]['max_temp'] }}&degC
+              {{ region['max_temp'] }}&degC
             </v-col>
             <v-col class="v-col px-0">
-              <v-icon size="large">{{weatherIcons[todayForecast[0]['weather']]}}</v-icon>
-              <span class="pa-2"> {{ weatherStatus[todayForecast[0]['weather']] }} </span>
+              <v-icon size="large">{{weatherIcons[region['weather']]}}</v-icon>
+              <span class="pa-2"> {{ weatherStatus[region['weather']] }} </span>
             </v-col>
           </v-row>
           <v-card-text class="px-0 pb-0">
             <v-icon>mdi-wind-power</v-icon>
-            <span class="px-2">Velocidad del viento: {{ todayForecast[0]['wind_speed'] }} - {{ todayForecast[0]['wind_speed_2'] }}</span>
+            <span class="px-2">Velocidad del viento: {{ region['wind_speed'] }} - {{ region['wind_speed_2'] }}</span>
           </v-card-text>
           <v-card-text class="px-0">
             <v-icon>mdi-weather-windy</v-icon>
-            <span class="px-2">Dirección del viento: {{ todayForecast[0]['wind_direction'] }}</span>
-          </v-card-text>
-        </v-card-text>
-
-
-      </v-card>
-      <v-card class="mx-md-3 px-1 mx-auto my-3 w-75" min-width="240">
-        <template v-slot:title>
-          <v-icon icon="mdi-weather-partly-cloudy"></v-icon>
-          <v-card-item title="Región Centro"></v-card-item>
-        </template>
-        <v-card-text class="py-0">
-          <v-row class="align-center">
-            <v-col class="text-h2">
-              {{ todayForecast[1]['max_temp'] }}&degC
-            </v-col>
-            <v-col class="v-col px-0">
-              <v-icon size="large">{{weatherIcons[todayForecast[1]['weather']]}}</v-icon>
-              <span class="pa-2"> {{ weatherStatus[todayForecast[1]['weather']] }} </span>
-            </v-col>
-          </v-row>
-          <v-card-text class="px-0 pb-0">
-            <v-icon>mdi-wind-power</v-icon>
-            <span class="px-2">Velocidad del viento: {{ todayForecast[1]['wind_speed'] }} - {{ todayForecast[1]['wind_speed_2'] }}</span>
-          </v-card-text>
-          <v-card-text class="px-0">
-            <v-icon>mdi-weather-windy</v-icon>
-            <span class="px-2">Dirección del viento: {{ todayForecast[1]['wind_direction'] }}</span>
-          </v-card-text>
-        </v-card-text>
-
-
-      </v-card>
-      <v-card class="mx-md-3 px-1 mx-auto my-3 w-75" min-width="240">
-        <template v-slot:title>
-          <v-icon icon="mdi-weather-partly-cloudy"></v-icon>
-          <v-card-item title="Región Sur"></v-card-item>
-        </template>
-        <v-card-text class="py-0">
-          <v-row class="align-center">
-            <v-col class="text-h2">
-              {{ todayForecast[2]['max_temp'] }}&degC
-            </v-col>
-            <v-col class="v-col px-0">
-              <v-icon size="large">{{weatherIcons[todayForecast[2]['weather']]}}</v-icon>
-              <span class="pa-2"> {{ weatherStatus[todayForecast[2]['weather']] }} </span>
-            </v-col>
-          </v-row>
-          <v-card-text class="px-0 pb-0">
-            <v-icon>mdi-wind-power</v-icon>
-            <span class="px-2">Velocidad del viento: {{ todayForecast[2]['wind_speed'] }} - {{ todayForecast[2]['wind_speed_2'] }}</span>
-          </v-card-text>
-          <v-card-text class="px-0">
-            <v-icon>mdi-weather-windy</v-icon>
-            <span class="px-2">Dirección del viento: {{ todayForecast[2]['wind_direction'] }}</span>
+            <span class="px-2">Dirección del viento: {{ region['wind_direction'] }}</span>
           </v-card-text>
         </v-card-text>
       </v-card>
+
+    </div>
+
+    <h2>Días próximos</h2>
+    <div v-if="nextForecast !== null">
+      <div v-for="forecast in nextForecast" class="forecast-container align-content-center pa-3 d-block">
+        <h3 class="pa-2">Fecha: {{ forecast[0]['date'] }}</h3>
+        <v-card class="d-md-flex justify-space-between v-row--no-gutters w-100">
+          <div v-for="data in forecast" class="v-col-auto pa-2">
+            <v-card-title>Region {{ data['region'] }}</v-card-title>
+            <v-card-subtitle>Temperatura min - max</v-card-subtitle>
+            <v-card-item class="text-h3" id="temp">{{ data['min_temp'] }}&degC - {{ data['max_temp'] }}&degC</v-card-item>
+            <v-card-subtitle>Estado del tiempo Mañana</v-card-subtitle>
+            <v-card-item>
+              <v-icon>{{ weatherIcons[data['weather']] }}</v-icon>
+              {{ weatherStatus[data['weather']] }}
+            </v-card-item>
+            <v-card-subtitle>Velocidad del viento - Direccion</v-card-subtitle>
+            <v-card-item class="d-flex justify-space-between text-justify">
+              <span><v-icon>mdi-wind-power</v-icon> {{ data['wind_speed'] }} - {{ data['wind_speed_2'] }} </span>
+              <span><v-icon>mdi-weather-windy</v-icon> {{ data['wind_direction'] }}</span>
+            </v-card-item>
+            <v-card-subtitle>Estado del tiempo Tarde</v-card-subtitle>
+            <v-card-item>
+              <v-icon>{{ weatherIcons[data['weather_2']] }}</v-icon>
+              {{ weatherStatus[data['weather_2']] }}
+            </v-card-item>
+            <v-card-subtitle>Velocidad del viento - Direccion</v-card-subtitle>
+            <v-card-item class="d-flex justify-space-between text-justify">
+              <span><v-icon>mdi-wind-power</v-icon> {{ data['wind_speed_3'] }} - {{ data['wind_speed_4'] }} </span>
+              <span><v-icon>mdi-weather-windy</v-icon> {{ data['wind_direction_2'] }}</span>
+            </v-card-item>
+          </div>
+        </v-card>
+      </div>
+    </div>
+    <div v-else>
+      <h3>No hay datos</h3>
     </div>
   </div>
+
 
 
 <!--  EJEMPLO-->
--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -11,12 +11,13 @@
 <template>
   <v-navigation-drawer v-model="drawer">
     <v-list>
-      <v-list-item title="VISOR DE PRONÓSTICOS" subtitle="Consultas"></v-list-item>
+      <v-list-item title="Menú" subtitle="Consultas"></v-list-item>
       <v-divider></v-divider>
-      <v-list-item link title="Report 1"></v-list-item>
+      <v-list-item class="mt-7 py-2" link title="Diario y futuros pronósticos"></v-list-item>
+      <v-list-item class="py-2" link title="Registro de pronósticos"></v-list-item>
     </v-list>
   </v-navigation-drawer>
-  <v-app-bar class="text-uppercase" title="Pronostico de los proximos dias">
+  <v-app-bar class="text-uppercase" title="Pronósticos">
     <template v-slot:prepend>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </template>
